3aa1df0a7cd3ebffe4472a97af4d7a557a222e04
leonardo339383 <llleonardoogomes@gmail.com>
2022-08-08 10:12:33 -0300

    Aula de angular 08/08/2022

--- src/app/components/pages/account/password/password.component.ts
+++ src/app/components/pages/account/password/password.component.ts
@@ -1,33 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+ 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent implements OnInit {
-
-  constructor() { }
-
+ 
+  mensagem: string = '';
+ 
+  constructor(
+    private usuarioService: UsuarioService,
+    private spinnerService: NgxSpinnerService
+  ) { }
+ 
   ngOnInit(): void {
   }
-
-   // construir a estrutura do nosso formulário
-   formPassword = new FormGroup({
-    //campo email
+ 
+  formPassword = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email])
   });
-
-  //função para acessar e exibir os erros de validação do formulário
-  get formularioPassword(): any{
+ 
+  get form(): any {
     return this.formPassword.controls;
   }
-
-  // função para capturar o SUBMIT do formulário
-  onSubmit() : void{
-    // exibindo os valores dos campos do formulário no console
-    console.log(this.formPassword.value);
+ 
+  onSubmit(): void {
+   
+    this.mensagem = '';
+    this.spinnerService.show();
+ 
+    this.usuarioService.postPassword(this.formPassword.value)
+      .subscribe({
+        next: (result) => {
+          this.mensagem = `Recuperação de senha para o usuário ${result.nome} realizada com sucesso.`;
+          this.spinnerService.hide();
+        },
+        error: (e) => {
+          console.log(e);
+          this.mensagem = 'Não foi possível realizar a recuperação da senha.';
+          this.spinnerService.hide();
+        }
+      })
   }
-
 }
+ 
+ 
+
